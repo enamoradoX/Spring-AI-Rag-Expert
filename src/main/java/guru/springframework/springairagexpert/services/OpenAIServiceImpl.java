4f7025409ef55e01d7b04a8e3c9fd3fe442f916b
GZ1Z2N <christian.enamorado@gm.com>
2025-12-14 12:40:26 -0500

    Rag AI expert working code.

--- src/main/java/guru/springframework/springairagexpert/services/OpenAIServiceImpl.java
+++ src/main/java/guru/springframework/springairagexpert/services/OpenAIServiceImpl.java
@@ -3,17 +3,16 @@ package guru.springframework.springairagexpert.services;
 import guru.springframework.springairagexpert.model.Answer;
 import guru.springframework.springairagexpert.model.Question;
 import lombok.RequiredArgsConstructor;
-import org.springframework.ai.chat.model.ChatModel;
-import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.chat.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.SearchRequest;
-import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Map;
 
@@ -24,16 +23,18 @@ import java.util.Map;
 @Service
 public class OpenAIServiceImpl implements OpenAIService {
 
-    final ChatModel chatModel;
-    final SimpleVectorStore vectorStore;
+    final ChatClient chatClient;
+    final VectorStore vectorStore;
 
-    @Value("classpath:/templates/rag-prompt-template-meta.st")
+    @Value("classpath:/templates/rag-prompt-template.st")
     private Resource ragPromptTemplate;
 
     @Override
     public Answer getAnswer(Question question) {
-        List<Document> documents = vectorStore.similaritySearch(SearchRequest.builder()
-                .query(question.question()).topK(5).build());
+        List<Document> documents = vectorStore.similaritySearch(
+                SearchRequest.query(question.question())
+                        .withTopK(5)
+        );
         List<String> contentList = documents.stream().map(Document::getContent).toList();
 
         PromptTemplate promptTemplate = new PromptTemplate(ragPromptTemplate);
@@ -42,7 +43,7 @@ public class OpenAIServiceImpl implements OpenAIService {
 
         contentList.forEach(System.out::println);
 
-        ChatResponse response = chatModel.call(prompt);
+        ChatResponse response = chatClient.call(prompt);
 
         return new Answer(response.getResult().getOutput().getContent());
     }
