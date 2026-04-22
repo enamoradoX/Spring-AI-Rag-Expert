b660a0e69f670bdc1b83bbaad2588b93288787a0
enamoradoX <enamoradoc1989@gmail.com>
2026-04-22 10:37:51 -0400

    Added angular front end, revised project structure, added CORS, revised gitIgnore.

---
+++ spring-ai-rag-expert-backend/src/main/java/guru/springframework/springairagexpert/services/OpenAIService.java
@@ -0,0 +1,8 @@
+package guru.springframework.springairagexpert.services;
+
+import guru.springframework.springairagexpert.model.Answer;
+import guru.springframework.springairagexpert.model.Question;
+
+public interface OpenAIService {
+    Answer getAnswer(Question question);
+}
---
+++ spring-ai-rag-expert-backend/src/main/java/guru/springframework/springairagexpert/services/OpenAIServiceImpl.java
@@ -0,0 +1,53 @@
+package guru.springframework.springairagexpert.services;
+
+import guru.springframework.springairagexpert.model.Answer;
+import guru.springframework.springairagexpert.model.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Service
+public class OpenAIServiceImpl implements OpenAIService {
+
+    final ChatModel chatModel;
+    final VectorStore vectorStore;
+
+    @Value("classpath:/templates/rag-prompt-template.st")
+    private Resource ragPromptTemplate;
+
+    @Override
+    public Answer getAnswer(Question question) {
+
+        List<Document> documents = vectorStore.similaritySearch(
+                SearchRequest.builder()
+                             .query(question.question())
+                             .topK(5)
+                             .build()
+        );
+
+        List<String> contentList = documents.stream()
+                                            .map(Document::getText)
+                                            .toList();
+
+        PromptTemplate promptTemplate = new PromptTemplate(ragPromptTemplate);
+
+        Prompt prompt = promptTemplate.create(
+                Map.of("input", question.question(), "documents", String.join("\n", contentList))
+        );
+
+        ChatResponse response = chatModel.call(prompt);
+
+        return new Answer(response.getResult().getOutput().getText());
+    }
+}
